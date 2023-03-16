855fd048da8759d5045dc42c49633cfcee968bdf
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-16 07:45:53 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - print a particular infomation
  *
  * Return: always 0
  */
 
-int main(void)
+int  main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
