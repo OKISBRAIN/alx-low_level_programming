6b14a776da950741aeb8e7a0bda59d579c3ba3f0
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-21 09:39:28 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints alphabets in lowercase
+ * Return: alaways 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+	_putchar('\n');
+}
