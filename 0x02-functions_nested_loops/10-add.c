7068bd8a58afc937e3a0e8472ccbfa4d778c2e40
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-21 20:34:18 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: parameter to be added
+ * @b: parameter to be added
+ * Return: sum(a + b)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
