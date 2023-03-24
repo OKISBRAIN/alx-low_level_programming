77c14b310306718a965e30d5d15264af5427d68f
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-23 19:43:26 -0700

    header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifdef MAIN_H
-#define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int _putchar(char c);
@@ -12,4 +10,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
-#endif
+
