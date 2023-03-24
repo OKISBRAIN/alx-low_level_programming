aec155026ef2f9d6273cdeed2e191004c0f465d6
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-23 18:16:19 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: parameter to be checked
+ * Return: 1 if is an uppercase character
+ * and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
