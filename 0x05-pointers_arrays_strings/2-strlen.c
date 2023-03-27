02c8907abce2365cb7c4ac41e328f8fb07732dd7
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-27 11:02:56 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,17 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string lenth to be printed
+ * @s: string length to be printed
  * Return: length of string
  */
 
 int _strlen(char *s)
 {
-	int i;
-	int count;
+	int i = 0;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	while (*(s + i) != 0)
 	{
-		count++;
+		i++;
 	}
-	return (count);
+	return (i);
 }
