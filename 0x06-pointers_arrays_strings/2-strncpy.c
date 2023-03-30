d499b7b17221e54c968aa1190bf4068d6cef8dc6
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-30 07:09:24 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
