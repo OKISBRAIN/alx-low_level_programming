b657dcf535c4abce062a373948643ee19921393c
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-04-03 20:22:44 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: always 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *j = needle;
+
+		while (*l == *j && *j != '\0')
+		{
+			l++;
+			j++;
+		}
+		if (*j == '\0')
+			return (haystack);
+	}
+	return (0);
+}
