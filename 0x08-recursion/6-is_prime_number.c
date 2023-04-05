9c5967d0d29c71b059cc5ef8163c7a9e4f6e1664
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-04-05 08:10:00 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that prints prime num
+ * @n: parameter
+ * Return: 1 if is a prime number and 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 10 == 1)
+		return (1);
+	return (0);
+}
